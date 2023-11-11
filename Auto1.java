484b7f550ad29358225e7a8a69cdb3289ca551ea
oesterer <oesterer@yahoo.com>
2023-11-10 18:23:59 -0800

    Update Auto1.java

--- Auto1.java
+++ Auto1.java
@@ -28,24 +28,54 @@ public class Auto1 extends DriveRobot {
 		waitForStart();
 		auto2();
 	}
+ 
+
+/*  Field Layout:
+
+Parking     /
+-----------/
+
+       --3--
+             |
+Robot        2
+             |
+       --1--
+
+*/
 
 	@Override void auto2() {
-        if(seeBlock(900)){
+		// Is there a team-prop straight ahead? (position 2)
+	        if(seeBlock(900)){
+	        // Move a bit to the side to avoid prop	
             strafe(100);
+            // Drive to line
             drive(740);/* At position 2 now (the one directly in front of start)*/
+            // Drop pixel by backing up
             drive(-200);
+            // Navigate to parking area
             park(-540, 0, 90);
         }else{
+        	// Move sideways to align with position 1
             strafe(320);
+            // Is there a team-prop straight ahead? (position 1)
             if(seeBlock(800)){
+            	// Drive to line
                 drive(550);/* At the edge of position 1 now */
+                // Drop pixel by backing up
                 drive(-200);
+                // Navigate to parking area
                 park(-340, 0, 90);
             }else{
+            	// Noting detected in pos 1 or 2 -> has to be in pos 3
+            	// Drive to align with pos 3
                 drive(590);
+                // Turn left
                 turn(90);
+                // Drive to the line
                 drive(440); /* at pos 3 now */
+                // Drop pixel by backing up
                 drive(-200);
+                // Navigate to parking area
                 park(0, -700,0);
             }
         }
